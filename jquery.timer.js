ac5d324f9389ef9e5b41a89db8afa8e097414f4b
= <james@james-brooks.net>
2011-04-12 08:14:01 +0100

    Made $.timer more library friendly.

--- jquery.timer.js
+++ jquery.timer.js
@@ -10,33 +10,35 @@
 	of jTuts.net
 */
 
-jQuery.timer = function(interval, callback, options) {
-	var options = jQuery.extend({ reset: 500 }, options); // Create options for the default reset value
-	var interval = interval || options.reset;
+(function($) {
+	jQuery.timer = function(interval, callback, options) {
+		var options = jQuery.extend({ reset: 500 }, options); // Create options for the default reset value
+		var interval = interval || options.reset;
 
-	if(!callback) { return false; }
+		if(!callback) { return false; }
 
-	timer = function(interval, callback) {
-		// Only used by internal code to call the callback
-		this.internalCallback = function() { callback(self); };
+		timer = function(interval, callback) {
+			// Only used by internal code to call the callback
+			this.internalCallback = function() { callback(self); };
 
-		// Clears any timers
-		this.stop = function() { clearInterval(self.id); };
-		// Resets timers to a new time
-		this.reset = function(time) {
-			if(self.id) { clearInterval(self.id); }
-			var time = time || options.reset;
+			// Clears any timers
+			this.stop = function() { clearInterval(self.id); };
+			// Resets timers to a new time
+			this.reset = function(time) {
+				if(self.id) { clearInterval(self.id); }
+				var time = time || options.reset;
 
-			this.id = setInterval(this.internalCallback, time);
+				this.id = setInterval(this.internalCallback, time);
+			};
+
+			// Set the interval time again
+			this.interval = interval;
+			this.id = setInterval(this.internalCallback, this.interval);
+
+			var self = this;
 		};
 
-		// Set the interval time again
-		this.interval = interval;
-		this.id = setInterval(this.internalCallback, this.interval);
-
-		var self = this;
+		// Create a new timer object
+		return new timer(interval, callback);
 	};
-
-	// Create a new timer object
-	return new timer(interval, callback);
-};
+})(jQuery);
